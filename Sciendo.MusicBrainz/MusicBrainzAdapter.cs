57d982d548423eb2e52063a3b7fe92702b4bb8dd
oduma <tavi_duma2002@yahoo.co.uk>
2017-03-09 09:14:37 +0000

    The basic of linking an esiting MBTrack with a localTrack not in a collection album works

--- Sciendo.MusicBrainz/MusicBrainzAdapter.cs
+++ Sciendo.MusicBrainz/MusicBrainzAdapter.cs
@@ -19,6 +19,7 @@ namespace Sciendo.MusicBrainz
         }
         public void LinkToExisting(IEnumerable<FileAnalysed> filesAnalysed)
         {
+
             foreach (var fileAnalysed in filesAnalysed)
             {
                 if (fileAnalysed.MarkedAsPartOfCollection)
@@ -31,12 +32,40 @@ namespace Sciendo.MusicBrainz
         }
 
         //for a non-collection album
+        //first check to see if the local path has not been already used and delete the old 
+        //localTrack. This assumes that the collection does not contain duplicate keys
+        //match(l:localTrack) where l.name="myfolderpath\\myfilepath.mp3" return l
+        //if not also check to see if the current track is not linked with other localTrack
+        //if found deleted
+        //mmatch (a:Album)-[r:PART_OF]-(b)-[r1:RELEASED_ON_MEDIUM]-(m:Cd)<-[*..3]-(ac:ArtistCredit) 
+        //where a.name=~"(?i)TRUE BLUE" and ac.name=~"(?i)Madonna" 
+        //with a, m, ac limit 1 match(m)<-[p: APPEARS_ON]-(t:Track)-[lp]-(l:localTrack) 
+        //where t.name=~"(?i)OPEN YOUR HEART" detach delete l
+        //after that go to create another one
         //match(a:Album)-[r: PART_OF]-(b)-[r1: RELEASED_ON_MEDIUM]-(m:Cd)<-[*..3]-(ac:ArtistCredit) where a.name=~"(?i)10CC" and ac.name=~"(?i)10CC" 
         //with a, m, ac limit 1 match(m)<-[p: APPEARS_ON]-(t:Track) where t.name=~"(?i)DONNA" 
         //MERGE(t)-[lr: HAVE_LOCAL]-(l:localTrack {name:'c:\\my path\\donna.mp3'})
         private void LinkToExistingNotCollection(FileAnalysed fileAnalysed)
         {
-           _graphClient.Cypher.Match(
+            _graphClient.Cypher.Match("(l:localTrack)")
+                .Where("l.name={filePath}")
+                .WithParam("filePath", fileAnalysed.FilePath.Replace(@"\", @"/"))
+                .DetachDelete("l").ExecuteWithoutResults();
+
+            _graphClient.Cypher.Match(
+                     "(a:Album)-[:PART_OF]-(b)-[:RELEASED_ON_MEDIUM]-(m:Cd)<-[*..3]-(ac:ArtistCredit)")
+                 .Where("a.name=~{albumName}")
+                 .WithParam("albumName", "(?ui).*" + fileAnalysed.Album + ".*")
+                 .AndWhere("ac.name=~{artistName}")
+                 .WithParam("artistName", "(?ui).*" + fileAnalysed.Artist + ".*")
+                 .With("a,m,ac")
+                 .Limit(1)
+                 .Match("(m)<-[:APPEARS_ON]-(t:Track)-[lp]-(l:localTrack)")
+                 .Where("t.name=~{trackName}")
+                 .WithParam("trackName", "(?ui).*" + fileAnalysed.Title + ".*")
+                 .DetachDelete("l").ExecuteWithoutResults();
+
+            _graphClient.Cypher.Match(
                     "(a:Album)-[:PART_OF]-(b)-[:RELEASED_ON_MEDIUM]-(m:Cd)<-[*..3]-(ac:ArtistCredit)")
                 .Where("a.name=~{albumName}")
                 .WithParam("albumName", "(?ui).*" + fileAnalysed.Album + ".*")
@@ -49,7 +78,7 @@ namespace Sciendo.MusicBrainz
                 .WithParam("trackName", "(?ui).*" + fileAnalysed.Title + ".*")
                 
                 //.Merge("(t)-[lr:HAVE_LOCAL]-(l:localTrack)");
-                .Merge("(t)-[lr: HAVE_LOCAL]-(l:localTrack {name:'" + fileAnalysed.FilePath.Replace(@"\",@"\\") + "'})").ExecuteWithoutResults();
+                .Merge("(t)-[lr: HAVE_LOCAL]-(l:localTrack {name:'" + fileAnalysed.FilePath.Replace(@"\",@"/") + "'})").ExecuteWithoutResults();
         }
 
         private void LinkToExistinginCollection(FileAnalysed fileAnalysed)
@@ -61,6 +90,12 @@ namespace Sciendo.MusicBrainz
         {
             throw new NotImplementedException();
         }
+    }
+
+    internal class LocalTrack
+    {
+        public string name { get; set; }
+//        public string id { get; set; }
     }
 
     public class MBRecord
